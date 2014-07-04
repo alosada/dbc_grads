7141effa8601ef53109a685ab75c32d770c73ca2
Alejandro E. Losada <alosada@gmail.com>
2014-07-04 01:07:00 -0700

    added linked in class

--- dbc_grads.rb
+++ dbc_grads.rb
@@ -4,19 +4,38 @@ require 'csv'
 CSV.open('dbc_grad_result.csv', 'wb') do |csv|
   csv << ['column1', 'column2'] #etc
 
-Grad = Struct.new(:name, :linkedin, :email, :location, :current, :past)
+Grad = Struct.new(:name, :linkedin, :email, :location, :current)
 
+class LinkedIn
 
+  def self.get_linked_in(grad)
+    client = LinkedIn::Client.new("KEY", "SECRETKEY")
+    client.authorize_from_access("USER_TOKEN","USER_SECRET")
+    begin
+      new_info = client.profile(:url => grad.linkedin , :fields => ['location:(name)', 'positions'])
+      location = clean_location(new_info.location.name)
+      grad.location = location
+      grad.current = new_info.positions.current
 
+      p '<<<<<<<<<<- Success ->>>>>>>>>>'
+    rescue StandardError => error
+      p "Error: #{error}"
+    end
+    #sleep Random.rand(300..1500)
+    return grad
+  end
 
-
-
-
-
-dbc_grads = CSV.foreach('dbc_test.csv') do |row|
-  grad=Grad.new(row[0], row[4], row[5])
-  p grad.name
-  p grad.linkedin
-  p grad.email
 end
+
+ale=Grad.new('Ale', 'http://linkedin.com/in/alosada', 'a@a.com')
+ale=LinkedIn.get_linked_in(ale)
+p ale.current
+p ale.location
+
+# dbc_grads = CSV.foreach('dbc_test.csv') do |row|
+#   grad=Grad.new(row[0], row[4], row[5])
+#   p grad.name
+#   p grad.linkedin
+#   p grad.email
+# end
 
