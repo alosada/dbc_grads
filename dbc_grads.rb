cabb26c6a3a4c0406ab23ba30e9213b416d235d4
Alejandro E. Losada <alosada@gmail.com>
2014-07-04 02:35:23 -0700

    dbc grad updater complete

--- dbc_grads.rb
+++ dbc_grads.rb
@@ -2,40 +2,54 @@ require 'linkedin'
 require 'csv'
 
 CSV.open('dbc_grad_result.csv', 'wb') do |csv|
-  csv << ['column1', 'column2'] #etc
+  csv << ['name', 'linkedin', 'email', 'location', 'title', 'company', 'start_date']
+end
 
-Grad = Struct.new(:name, :linkedin, :email, :location, :current)
+Grad = Struct.new(:name, :linkedin, :email, :location, :title, :company, :start_date)
 
-class LinkedIn
+class Linked_In
 
   def self.get_linked_in(grad)
-    client = LinkedIn::Client.new("KEY", "SECRETKEY")
-    client.authorize_from_access("USER_TOKEN","USER_SECRET")
+    client = LinkedIn::Client.new("key", "sekrit key")
+    client.authorize_from_access("user token", "user sekrit")
     begin
       new_info = client.profile(:url => grad.linkedin , :fields => ['location:(name)', 'positions'])
-      location = clean_location(new_info.location.name)
+      location = new_info.location.name
       grad.location = location
-      grad.current = new_info.positions.current
-
+      grad.title = new_info.positions.all[0].title
+      grad.company = new_info.positions.all[0].company.name
+      grad.start_date = "#{new_info.positions.all[0].start_date.month}, #{new_info.positions.all[0].start_date.year}"
       p '<<<<<<<<<<- Success ->>>>>>>>>>'
     rescue StandardError => error
       p "Error: #{error}"
     end
-    #sleep Random.rand(300..1500)
+    sleep Random.rand(0.3..1.5)
     return grad
   end
 
 end
 
-ale=Grad.new('Ale', 'http://linkedin.com/in/alosada', 'a@a.com')
-ale=LinkedIn.get_linked_in(ale)
-p ale.current
-p ale.location
+def add_to_csv(grad)
+  CSV.open('dbc_grad_result.csv', 'a+') do |csv|
+    csv << [grad.name, grad.linkedin, grad.email, grad.location, grad.title, grad.company, grad.start_date]
+  end
+end
 
-# dbc_grads = CSV.foreach('dbc_test.csv') do |row|
-#   grad=Grad.new(row[0], row[4], row[5])
-#   p grad.name
-#   p grad.linkedin
-#   p grad.email
-# end
+def fix_url(url)
+  unless /http:\/\//.match(url)
+    url = 'http://' + url
+  end
+  url
+end
+
+def make_grad(row)
+  li_url = fix_url(row[4]
+  Grad.new(row[0], li_url, row[5])
+end
+
+dbc_grads = CSV.foreach('dbc_test.csv') do |row|
+  grad=make_grad(row)
+  grad=Linked_In.get_linked_in(grad)
+  add_to_csv(grad)
+end
 
